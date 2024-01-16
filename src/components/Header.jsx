1e4cd457009ea4a7d65172ad29dfe4055c0652f9
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-01-16 11:24:35 +0000

    First commit. Header component created but not complete.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../css/header.css'
+
+export default function Header(){
+    return(
+        <>
+        <div id='header-container' className="bg-[url('src/assets/header-img.gif')] w-full md: sm:w-auto xl:w-auto l:w-auto bg-no-repeat bg-cover">
+        <h1>Hi, I'm Dan</h1>
+        </div>
+        </>
+    )
+}

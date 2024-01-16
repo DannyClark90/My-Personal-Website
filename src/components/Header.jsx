3e9929a052a37d5ac946d12840b0442fe29bdc80
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-01-16 13:41:39 +0000

    Header complete & responsive.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,15 @@ import '../css/header.css'
 export default function Header(){
     return(
         <>
-        <div id='header-container' className="bg-[url('src/assets/header-img.gif')] w-full md: sm:w-auto xl:w-auto l:w-auto bg-no-repeat bg-cover">
-        <h1>Hi, I'm Dan</h1>
+        <div id='header-container' className="bg-cover bg-no-repeat relative">
+        <img src="src/assets/header-img.gif" alt="Desk with houseplants on left and computer monitor and lamp on right" className='w-full h-full' />
+        <h1 className='absolute mb-6 
+        xs:mr-24 xs:pr-4 xs:text-xs xs:mb-4 
+        sm:mr-40 sm:pr-4 sm:text-s 
+        md:mr-64 md:pr-4 md:text-m 
+        lg:mr-80 lg:pr-7 lg:text-l 
+        xl:mr-96 xl:pr-10 xl:text-xl 
+        2xl:mr-96 2xl:pr-28 2xl:text-2xl'>Hi, I'm Dan</h1>
         </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens:{
+      'xs': '375px',
+      'sm': '640px',
+      'md': '769px',
+      'lg': '1024px',
+      'xl': '1536px'
+    },
+    fontSize: {
+      'xs': '2.2rem',
+      's': '3.6rem',
+      'm': '4.6rem',
+      'l': '6rem',
+      'xl': '7.2rem',
+      '2xl': '8.5rem'
+    },
     extend: {},
   },
   plugins: [],

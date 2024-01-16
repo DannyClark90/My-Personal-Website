607d7b088cbc2d02c4205e979c01e36330ac7de9
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-01-16 14:46:32 +0000

    Header subtitle responsive formatting tweaked.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,16 @@ export default function Header(){
             
             <h1 className='absolute mb-6 
             xs:mr-24 xs:pr-4 xs:text-xs
-            sm:mr-40 sm:pr-4 sm:text-s 
-            md:mr-64 md:pr-4 md:text-m 
-            lg:mr-80 lg:pr-7 lg:text-l 
-            xl:mr-96 xl:pr-10 xl:text-xl 
-            2xl:mr-96 2xl:pr-28 2xl:text-2xl'>Hi, I'm Dan</h1>
+            sm:mr-40 sm:mb-14 sm:pr-8 sm:text-s 
+            md:mr-64 md:pr-6 md:text-m 
+            lg:mr-80 lg:pr-18 lg:text-l 
+            xl:mr-96 xl:pr-40 xl:text-xl'>Hi, I'm Dan</h1>
             <h2 id='subtitle' className="absolute
             xs:mt-6 xs:mr-24 xs:pr-4 xs:text-6xs
-            sm:mt-8 sm:mr-40 sm:pr-4 sm:text-5xs 
-            md:mt-18 md:mr-64 md:pr-4 md:text-4xs 
-            lg:mt-18 lg:mr-80 lg:pr-7 lg:text-3xs 
-            xl:mt-24 xl:mr-96 xl:pr-10 xl:text-xxs">Trainee Software Engineer</h2>
+            sm:mt-8 sm:mr-40 sm:pr-8 sm:text-5xs 
+            md:mt-20 md:mr-64 md:pr-6 md:text-4xs 
+            lg:mt-28 lg:mr-80 lg:pr-18 lg:text-3xs 
+            xl:mt-30 xl:mr-96 xl:pr-40 xl:text-xxs">Trainee Software Engineer</h2>
         </div>
         </>
     )

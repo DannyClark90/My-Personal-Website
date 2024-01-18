5f8b76a1c083d0926acd84d3f4eb3e6ea08a6027
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-01-18 13:08:30 +0000

    Mobile nav menu animation added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,32 @@ import { motion } from "framer-motion"
 
 export default function NavBar() {
   const [click, setClick] = useState(true)
-  const [open, setOpen] = useState('menuClosed');
 
   const toggleMenu = () => {
     setClick(!click)
-    setOpen(!open);
   };
+
+  const menuVars = {
+    initial:{
+      scaleY:0
+    },
+    animate: {
+      scaleY:1,
+      transition: {
+        duration: 0.5
+      }
+    },
+    exit:{ 
+        scaleY:0,
+        transition: {
+          duration: 0.5
+        }
+      }
+    }
 
   const content = (
     <>
-      <div id="menuOpen" className="xs:flex xs:gap-5 md:hidden">
-        <ul className="gap-1">
+        <motion.ul variants={menuVars} initial="initial" animate="animate" exit="exit" id="menuOpen" className="md:hidden xs:flex origin-top">
           <Link spy={true} smooth={true} className="hover:text-gray-50 pt-2">
             <li>Bio</li>
           </Link>
@@ -40,22 +55,21 @@ export default function NavBar() {
           <Link spy={true} smooth={true} className="hover:text-gray-50 pt-2">
             <li>Employment History</li>
           </Link>
-        </ul>
-      </div>
+        </motion.ul>
     </>
   );
 
   return (
     <>
       <nav id="menuClosed">
-        <div className="pl-20">
+        <div className="pl-10">
           <button className="text-xs py-2 lg:hidden xs:flex transition" onClick={toggleMenu}>
             {click ? <FaBars /> : <FaTimes/>}
             
           </button>
-          <p className="text-3xs sm:hidden lg:flex">Menu</p>
+          <p className="text-3xs xs:hidden lg:flex">Menu</p>
         </div>
-        <div className="sm:hidden lg:flex">
+        <div className="xs:hidden lg:flex">
           <ul id="navItems">
             <Link spy={true} smooth={true} className="hover:text-gray-50 pt-2 cursor-pointer">
               <li>Bio</li>
